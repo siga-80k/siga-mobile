4888c3bafa18194625bbb4a78dd8411a39a9b85a
Wilson Santos Carvalho <wilson.carvalho@Mac-mini-de-Ayo.local>
2020-06-14 17:22:38 -0300

    picker added on contacts form

--- src/pages/ContactsForm/index.js
+++ src/pages/ContactsForm/index.js
@@ -1,42 +1,73 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {View, Platform, ActionSheetIOS} from 'react-native';
 import {Form} from '@unform/mobile';
+import {Picker} from '@react-native-community/picker';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 // Components
 import Input from '../../components/Input';
 import SubmitButton from '../../components/SubmitButton';
 
-import {Container, AlertTitle, AlertSubTitle} from './styles';
+import {
+  Container,
+  AlertTitle,
+  AlertSubTitle,
+  PickerContainer,
+  PickerText,
+} from './styles';
 
 const ContactsForm = () => {
+  const [result, setResult] = useState('Selecione a forma de contato');
+
+  const onPress = () =>
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['Cancel', 'Telefone', 'E-mail'],
+
+        cancelButtonIndex: 0,
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 0) {
+        } else if (buttonIndex === 1) {
+          setResult('Telefone');
+        } else if (buttonIndex === 2) {
+          setResult('E-mail');
+        }
+      },
+    );
+
   return (
     <Container>
       <AlertTitle>Insira seu contato de segurança</AlertTitle>
       <AlertSubTitle>
         O sistema irá informar a situação para seu contato em caso de emergência
       </AlertSubTitle>
+      {Platform.OS === 'ios' ? (
+        <PickerContainer onPress={onPress}>
+          <View style={{marginRight: 10}}>
+            {result === 'Telefone' ? (
+              <MaterialIcons name="contact-phone" size={20} color="#666" />
+            ) : (
+              <MaterialIcons name="contact-mail" size={20} color="#666" />
+            )}
+          </View>
+          <PickerText>{result}</PickerText>
+        </PickerContainer>
+      ) : (
+        <></>
+      )}
+
       <Form>
         <Input
           autoCorrect={false}
-          secureTextEntry
           autoCapitalize="none"
           keyboardType="email-address"
           name="user"
           icon="contacts"
-          placeholder="Telefone ou e-mail"
+          placeholder={result}
           returnKeyType="next"
           onSubmitEditing={() => {}}
         />
-        <Input
-          autoCorrect={false}
-          secureTextEntry
-          autoCapitalize="none"
-          keyboardType="email-address"
-          name="user"
-          icon="lock"
-          placeholder="Confirme sua palavra-chave"
-          returnKeyType="next"
-          onSubmitEditing={() => {}}
-        />
+
         <SubmitButton onPress={() => {}}>Cadastrar</SubmitButton>
       </Form>
     </Container>
--- src/pages/ContactsForm/styles.js
+++ src/pages/ContactsForm/styles.js
@@ -23,3 +23,25 @@ export const AlertSubTitle = styled.Text`
   /* margin-top: 30px; */
   margin: 0px 15px 20px 15px;
 `;
+
+export const PickerContainer = styled.TouchableOpacity`
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  background: #fff;
+  border-radius: 10px;
+  margin-bottom: 8px;
+
+  border: 1px solid;
+
+  border-color: #ccc;
+
+  flex-direction: row;
+  align-items: center;
+`;
+export const PickerText = styled.Text`
+  flex: 1;
+  color: #1e252d;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
