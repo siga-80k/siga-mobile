21c9d5ccafc2da87c0f650ed616b95826dafc3b2
Wilson Santos Carvalho <wilson.carvalho@Mac-mini-de-Ayo.local>
2020-06-14 09:52:13 -0300

    connection screen created

--- src/pages/Connection/index.js
+++ src/pages/Connection/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Alert} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-// import { Container } from './styles';
+import Map from '../../assets/map.jpg';
 
-const Connection = () => {
-  return <View />;
-};
+import {
+  SafeAreaWrapper,
+  Container,
+  AlertTitle,
+  ButtonRadius,
+  ButtonContainer,
+  BGMap,
+  AlertSubTitle,
+} from './styles';
 
-export default Connection;
+export default function Connection({navigation}) {
+  return (
+    <SafeAreaWrapper>
+      <BGMap source={Map} imageStyle={{opacity: 0.3}}>
+        <Container>
+          <AlertTitle>Pressione para estabelecer conexão</AlertTitle>
+          <ButtonContainer
+            onPress={() => {
+              Alert.alert('Conectado!', 'O módulo está pronto para ser usado');
+            }}>
+            <ButtonRadius>
+              <View style={{margin: 10, marginBottom: 0}}>
+                <MaterialCommunityIcons
+                  name="bluetooth-connect"
+                  size={180}
+                  color="#009bdb"
+                />
+              </View>
+            </ButtonRadius>
+          </ButtonContainer>
+          <AlertSubTitle>
+            A conexão será estabelecida automaticamente
+          </AlertSubTitle>
+        </Container>
+      </BGMap>
+    </SafeAreaWrapper>
+  );
+}
